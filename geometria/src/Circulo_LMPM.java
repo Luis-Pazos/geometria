fea38765797a46d40d2f23441a7c6e461586d2f5
luismiguelpazosmoreno <lmpazos77@gmail.com>
2020-04-30 21:59:08 +0200

    Siempre positivo

--- geometria/src/Circulo_LMPM.java
+++ geometria/src/Circulo_LMPM.java
@@ -1,6 +1,10 @@
 
 public class Circulo_LMPM extends FiguraGeometrica_LMPM {
 	private double radio;
+	
+	//Convertir negativo a positivo
+	if radio <0 then radio = (radio*-1);
+			
 	//Constante pi
 	static final double PI = 3.1416;
 	
